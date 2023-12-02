cd1782f108af1b0c091e15a8926c4b7a4babf2cd
Yasmin0909 <yasmin.tresso@etec.sp.gov.br>
2023-12-02 05:14:55 -0300

    Configurando wordpress

---
+++ news/wp-content/plugins/the-post-grid/assets/blocks/main.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9641fdb6f8a04b71dad6');
